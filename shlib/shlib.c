817ff8d6482224e7e6c4b02a26f3cb62f4d522fa
Andrew Chambers <ac@acha.ninja>
2019-05-25 22:08:37 +1200

    Add a simple command completion heuristic.

--- shlib/shlib.c
+++ shlib/shlib.c
@@ -187,7 +187,7 @@ static Janet glob_(int32_t argc, Janet *argv) {
 
   janet_fixarity(argc, 1);
   const char *pattern = janet_getcstring(argv, 0);
-  if (glob(pattern, GLOB_ERR|GLOB_NOCHECK, NULL, &g) != 0)
+  if (glob(pattern, GLOB_NOCHECK, NULL, &g) != 0)
     panic_errno("glob", errno);
 
   char **p = g.gl_pathv;
