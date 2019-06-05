7d0c52741593b7220c724d895e4c55dd4ef86759
Adam Schwalm <adamschwalm@gmail.com>
2019-06-04 23:21:08 -0500

    Use latest janet version for ci

--- support/ci.sh
+++ support/ci.sh
@@ -2,7 +2,7 @@
 
 set -uex
 
-janetver="6e8beff0a0eb829eb4f4f55df53d97c5a9815e29"
+janetver="cc1ff9125ad504b3d9844624e0c31f56b2ba759b"
 janeturl="https://github.com/janet-lang/janet/archive/${janetver}.tar.gz"
 mkdir -p ci_builds
 prefix="$(readlink -f ./ci_builds)/installed"
